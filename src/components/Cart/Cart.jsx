1c581f6139328820d66e67e2d7322523dcc74b44
Amr Hesham <msam.team20@gmail.com>
2023-09-19 13:53:46 +0300

    Enhancements

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,13 +1,12 @@
 import React, { useContext, useEffect, useState } from "react";
-import image from "../../Assets/1678305677165-cover.jpeg";
 import { CartContext } from "../../context/CartContext";
 import { toast } from "react-toastify";
 import { Triangle } from "react-loader-spinner";
-import { date } from "yup";
 
 const Cart = () => {
   const [cartItems, setCartItems] = useState(null);
   const [cartPrice, setCartPrice] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
   const {
     getLoggedUserCart,
     removeFromCart,
@@ -19,6 +18,7 @@ const Cart = () => {
   async function removeAllCart() {
     const { data } = await clearCart();
     setCartItems(null);
+    setCartPrice(0);
     await setCartCount(data?.numOfCartItems);
     toast("Cart Cleard Successfully", {
       type: "success",
@@ -26,12 +26,17 @@ const Cart = () => {
       hideProgressBar: false,
     });
   }
-  async function getUserCart(prodId) {
+  async function getUserCart() {
+    setIsLoading(true);
     const { data } = await getLoggedUserCart();
-    if (data?.status == "success") {
+
+    if (data?.status === "success") {
+      await setIsLoading(false);
       await setCartCount(data?.numOfCartItems);
       await setCartItems(data?.data?.products);
       await setCartPrice(data?.data.totalCartPrice);
+    } else {
+      setIsLoading(false);
     }
   }
   async function RemoveFromCart(prodId) {
@@ -69,68 +74,74 @@ const Cart = () => {
             Clear Cart
           </button>
 
-          {cartItems?.map((item) => (
-            <div
-              className="row align-items-center py-2 border-bottom "
-              key={item.product._id}
-            >
-              <div className="col-md-8">
-                <div className="box d-flex align-items-center gap-3">
-                  <img
-                    src={item.product.imageCover}
-                    alt={item.product.title}
-                    className="w-25"
-                  />
-
-                  <div className="info">
-                    {" "}
-                    <h3 className="h5">{item.product.title}</h3>
-                    <p className="text-main">{item.price} EGP</p>
-                    <p className="text-main fw-bold">
-                      {item.product.category.name}
-                    </p>
-                    <button
-                      className="btn text-white bg-main cursor-pointer"
-                      onClick={() => {
-                        RemoveFromCart(item.product._id);
-                      }}
-                    >
-                      <i className="fa-solid fa-xmark"></i> Remove
-                    </button>
-                  </div>
-                </div>
-              </div>
-              <div className="col-md-4">
-                <div className="quantity d-flex align-items-center justify-content-center">
-                  <button
-                    className="btn btn-sm bg-main text-white font-sm"
-                    onClick={() => {
-                      updateItemsCount(item.product._id, item.count + 1);
-                    }}
-                  >
-                    +
-                  </button>
-                  <span className="fw-bold d-inline-block mx-3">
-                    {item.count}
-                  </span>
-                  <button
-                    className="btn btn-sm bg-main text-white font-sm"
-                    disabled={item.product.count > 0 ? false : true}
-                    onClick={() => {
-                      updateItemsCount(item.product._id, item.count - 1);
-                      console.log(item.count);
-                    }}
-                  >
-                    -
-                  </button>
-                </div>
-              </div>
-            </div>
-          )) || (
+          {isLoading ? (
             <div className="d-flex align-items-center justify-content-center">
               <Triangle />
-              <h1 className="text-black-50 ms-3">No Items In Your Cart</h1>
             </div>
+          ) : (
+            <>
+              {cartItems?.length > 0 ? (
+                cartItems.map((item) => (
+                  <div
+                    className="row align-items-center py-2 border-bottom "
+                    key={item.product._id}
+                  >
+                    <div className="col-md-8">
+                      <div className="box d-flex align-items-center gap-3">
+                        <img
+                          src={item.product.imageCover}
+                          alt={item.product.title}
+                          className="w-25"
+                        />
+
+                        <div className="info">
+                          {" "}
+                          <h3 className="h5">{item.product.title}</h3>
+                          <p className="text-main">{item.price} EGP</p>
+                          <p className="text-main fw-bold">
+                            {item.product.category.name}
+                          </p>
+                          <button
+                            className="btn text-white bg-main cursor-pointer"
+                            onClick={() => {
+                              RemoveFromCart(item.product._id);
+                            }}
+                          >
+                            <i className="fa-solid fa-xmark"></i> Remove
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="col-md-4">
+                      <div className="quantity d-flex align-items-center justify-content-center">
+                        <button
+                          className="btn btn-sm bg-main text-white font-sm"
+                          onClick={() => {
+                            updateItemsCount(item.product._id, item.count + 1);
+                          }}
+                        >
+                          +
+                        </button>
+                        <span className="fw-bold d-inline-block mx-3">
+                          {item.count}
+                        </span>
+                        <button
+                          className="btn btn-sm bg-main text-white font-sm"
+                          disabled={item.count !== 1 ? false : true}
+                          onClick={() => {
+                            updateItemsCount(item.product._id, item.count - 1);
+                          }}
+                        >
+                          -
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                ))
+              ) : (
+                <h1 className="h5 text-center">No Items To Show</h1>
+              )}
+            </>
           )}
         </div>
       </div>
--- src/components/WishList/WishList.jsx
+++ src/components/WishList/WishList.jsx
@@ -5,6 +5,8 @@ import { toast } from "react-toastify";
 import { Triangle } from "react-loader-spinner";
 
 const WishList = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [wishItems, setWishItems] = useState(null);
   const {
     GetUserWishList,
     RemoveFromWishList,
@@ -13,12 +15,17 @@ const WishList = () => {
   } = useContext(WishListContext);
   const { addToCart, getLoggedUserCart, setCartCount } =
     useContext(CartContext);
-  const [wishItems, setWishItems] = useState(null);
 
   async function getUserWishList() {
+    await setIsLoading(true);
     const { data } = await GetUserWishList();
-    await setWishListCount(data?.count);
-    await setWishItems(data?.data);
+    if (data.status === "success") {
+      setIsLoading(false);
+      await setWishListCount(data?.count);
+      await setWishItems(data?.data);
+    } else {
+      setIsLoading(false);
+    }
   }
   async function removeFromWishList(prodId) {
     const { data } = await RemoveFromWishList(prodId);
@@ -46,62 +53,69 @@ const WishList = () => {
     <div className="cartPage py-2 h-100vh ">
       <div className="container bg-main-light rounded p-4">
         <h3 className="text-main h5">Wish List Items ({wishListCount})</h3>
-        {wishItems?.map((item) => (
-          <div
-            className="wishItem d-flex align-items-center border-bottom py-2"
-            key={item.id}
-          >
-            <div className="row align-items-center">
-              <div className="col-md-2">
-                <img src={item.imageCover} alt={item.title} className="w-100" />
-              </div>
-              <div className="col-md-10">
-                <div className="info">
-                  <h4 className="mb-2">{item.title}</h4>
-                  <p className="mb-2">{item.description}</p>
-                  <span className="d-block mb-2 text-main">
-                    Price: <strong>{item.price} EGP</strong>
-                  </span>
-                  <span className="d-block mb-2 fw-bold text-black-50">
-                    Sold: {item.sold}{" "}
-                  </span>
-                  <span className="d-block mb-2">
-                    Rating:
-                    <span>
-                      <i className="fa-solid fa-star rating-color me-1 ms-2"></i>
-                      {item.ratingsAverage}
+        {isLoading ? (
+          <div className="d-flex align-items-center justify-content-center">
+            <Triangle />
+          </div>
+        ) : wishItems?.length > 0 ? (
+          wishItems.map((item) => (
+            <div
+              className="wishItem d-flex align-items-center border-bottom py-2"
+              key={item.id}
+            >
+              <div className="row align-items-center">
+                <div className="col-md-2">
+                  <img
+                    src={item.imageCover}
+                    alt={item.title}
+                    className="w-100"
+                  />
+                </div>
+                <div className="col-md-10">
+                  <div className="info">
+                    <h4 className="mb-2">{item.title}</h4>
+                    <p className="mb-2">{item.description}</p>
+                    <span className="d-block mb-2 text-main">
+                      Price: <strong>{item.price} EGP</strong>
                     </span>
-                  </span>
-                  <span className="d-block mb-2">
-                    Category: {item.category.name}
-                  </span>
-                  <div className="actions">
-                    <button
-                      className="btn bg-main text-white me-3"
-                      onClick={() => {
-                        removeFromWishList(item._id);
-                      }}
-                    >
-                      <i className="fa-solid fa-trash"></i> Remove
-                    </button>
-                    <button
-                      className="btn bg-main text-white"
-                      onClick={() => {
-                        addToCartFromWishList({ productId: item._id });
-                      }}
-                    >
-                      <i className="fa-solid fa-cart-plus"></i> Add To Cart
-                    </button>
+                    <span className="d-block mb-2 fw-bold text-black-50">
+                      Sold: {item.sold}{" "}
+                    </span>
+                    <span className="d-block mb-2">
+                      Rating:
+                      <span>
+                        <i className="fa-solid fa-star rating-color me-1 ms-2"></i>
+                        {item.ratingsAverage}
+                      </span>
+                    </span>
+                    <span className="d-block mb-2">
+                      Category: {item.category.name}
+                    </span>
+                    <div className="actions">
+                      <button
+                        className="btn bg-main text-white me-3"
+                        onClick={() => {
+                          removeFromWishList(item._id);
+                        }}
+                      >
+                        <i className="fa-solid fa-trash"></i> Remove
+                      </button>
+                      <button
+                        className="btn bg-main text-white"
+                        onClick={() => {
+                          addToCartFromWishList({ productId: item._id });
+                        }}
+                      >
+                        <i className="fa-solid fa-cart-plus"></i> Add To Cart
+                      </button>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-        )) || (
-          <div className="d-flex align-items-center justify-content-center">
-            <Triangle />
-            <h1 className="text-black-50 ms-3">No Items In Your Wish List</h1>
-          </div>
+          ))
+        ) : (
+          <h1 className="h5 text-center">No Items To Show</h1>
         )}
       </div>
     </div>
